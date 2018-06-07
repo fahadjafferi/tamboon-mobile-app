ee7164a7ab793f455fc4074eb8fe6ca065931d56
Fahad <fahad.jafferi@gmail.com>
2018-06-07 10:47:47 +0700

    Initial Commit

---
+++ tamboon-mobile-appTests/tamboon_mobile_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  tamboon_mobile_appTests.swift
+//  tamboon-mobile-appTests
+//
+//  Created by FAHAD JAFFERI on 7/6/18.
+//  Copyright © 2018 FAHAD JAFFERI. All rights reserved.
+//
+
+import XCTest
+@testable import tamboon_mobile_app
+
+class tamboon_mobile_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
